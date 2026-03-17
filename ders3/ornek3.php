77bae54dc23b2b645c991f635609b329c8cd7947
gokceKy <gaia.gkce@gmail.com>
2026-03-17 11:19:55 +0300

    ders3

--- ders3/ornek3.php
+++ ders3/ornek3.php
@@ -63,23 +63,103 @@ echo "<h2>For Loop ile Sayı Dizisi</h2>";
 
 // Boş dizi oluşturma
 $numbers = array();
-
 // For döngüsü ile diziye sayılar ekleme
 for ($i = 1; $i <= 10; $i++) {
- 
-      $numbers[] = $i;
-    
+      $numbers[] = $i;  
 }
-//Çift sayılar $i%2==0
-// Tek Sayılar
-//$toplam =0;
-//1 den 10 a kadar sayıların toplamı
-// döngüde sayı 5 de dursun 
-// Diziyi yazdırma
-// tersten yazma
 for ($i = 0; $i < count($numbers); $i++) {
     echo "Number: " . $numbers[$i] . "<br>";
+}
+echo "<h2>Çift Sayılar</h2>";
+for ($i = 1; $i <= 10; $i++) {
+    if ($i % 2 == 0) {
+        echo $i . "<br>";
+    }
+}
+echo "<h2>Tek Sayılar</h2>";
+for ($i = 1; $i <= 10; $i++) {
+    if ($i % 2 != 0) {
+        echo $i . "<br>";
+    }
+}
 
+echo "<h2>1'den 10'a kadar sayıların toplamı</h2>";
+$toplam = 0;
+
+for ($i = 1; $i <= 10; $i++) {
+    $toplam += $i;
+    $toplam = $toplam + $i;
+}
+
+echo "Toplam: " . $toplam . "<br>";
+
+
+echo "<h2>Döngüde sayı 5'te dursun</h2>";
+for ($i = 1; $i <= 10; $i++) {
+
+    if ($i == 5) {
+        break;
+    }
+
+    echo $i . "<br>";
+}
+
+
+echo "<h2>Dizi Yazdırma</h2>";
+
+$numbers = array(1,2,3,4,5,6,7,8,9,10);
+
+foreach ($numbers as $num) {
+    echo $num . "<br>";
+}
+
+
+echo "<h2>Tersten Yazma</h2>";
+
+for ($i = 10; $i >= 1; $i--) {
+    echo $i . "<br>";
+}
+echo "<h2>Dizi Yazdırma</h2>";
+
+$numbers = array(1,2,3,4,5,6,7,8,9,10);
+
+foreach ($numbers as $num) {
+    echo $num . "<br>";
+}
+
+echo "<h2>Tersten Yazma</h2>";
+
+for ($i = 10; $i >= 1; $i--) {
+    echo $i . "<br>";
+}
+
+/* ===== BENZER ÖRNEKLER ===== */
+
+echo "<h2>3'ün Katları</h2>";
+
+for ($i = 1; $i <= 10; $i++) {
+    if ($i % 3 == 0) {
+        echo $i . "<br>";
+    }
+}
+
+
+echo "<h2>Sayıların Kareleri</h2>";
+
+for ($i = 1; $i <= 10; $i++) {
+    echo $i . " karesi: " . ($i * $i) . "<br>";
+}
+
+
+echo "<h2>5'i Atla (continue örneği)</h2>";
+
+for ($i = 1; $i <= 10; $i++) {
+
+    if ($i == 5) {
+        continue;
+    }
+
+    echo $i . "<br>";
 }
 
 /* =====================================
